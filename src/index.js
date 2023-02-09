ec0b2c3b2ea86e4cb07c32d092ac306ffb015a50
Christian Hernandez <hernandezchristian94@gmail.com>
2023-02-08 18:07:02 -0800

    complete lab

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const dogBar = document.querySelector("#dog-bar");
+const dogInfo = document.querySelector("#dog-info");
+const pupsUrl = "http://localhost:3000/pups";
+
+fetch(pupsUrl)
+  .then((resp) => resp.json())
+  .then((pupData) => pupData.forEach(renderPups));
+
+renderPups = (pups) => {
+  const pupNameList = document.createElement("span");
+  pupNameList.textContent = pups.name;
+  dogBar.append(pupNameList);
+
+  pupNameList.addEventListener("click", () => {
+    fetch(`http://localhost:3000/pups/${pups.id}`)
+      .then((response) => response.json())
+      .then((pupData) => pupsDisplay(pupData));
+  });
+};
+
+pupsDisplay = (pups) => {
+  dogInfo.innerHTML = "";
+
+  const pupImg = document.createElement("img");
+  pupImg.src = pups.image;
+  const pupName = document.createElement("h2");
+  pupName.textContent = pups.name;
+  const pupBtn = document.createElement("button");
+  isGoodDog = () => {
+    if (pups.isGoodDog === true) {
+      return (pupBtn.textContent = "Is good dog!");
+    } else {
+      return (pupBtn.textContent = "Is bad dog");
+    }
+  };
+  isGoodDog();
+
+  pupBtn.addEventListener("click", (e) => {
+    fetch(`http://localhost:3000/pups/${pups.id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        isGoodDog: !pups.isGoodDog,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => pupsDisplay(json));
+  });
+
+  dogInfo.append(pupImg, pupName, pupBtn);
+};
